8fb2aef0e4dae4546f8062f962e3e195c0252bda
DanielePilati <165797370+DanielePilati@users.noreply.github.com>
2024-08-01 13:37:58 +0200

    init

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
+	@GetMapping("/greetings")
+	public String greetings(@RequestParam(name = "name") String name, Model model) {
+		
+		
+		
+		return new String();
+	}
+	
+	
 }
